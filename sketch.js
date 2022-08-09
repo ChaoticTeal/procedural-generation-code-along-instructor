a2909d48f9627f8b2f097f39d3b258ae7bf8377f
ChaoticTeal <jawhittom@gmail.com>
2022-08-09 16:40:50 -0500

    Separates class definitions, adds island generation

--- sketch.js
+++ sketch.js
@@ -1,54 +1,143 @@
+// Use a variable to define our canvas width and height
+// This makes it easier to adjust and reference dimensions in code
 let canvasDimensions = 500;
+// A reference to the main canvas
+let canvas;
+// The number of rows and columns we want our grid to have
 let boxes = 50;
-let generationPasses = 5;
-let onChance = 0.2;
-let neighborModifier = 0.05;
-let offColor;
-let onColor;
+// The number of water boxes we want around the outside of whatever we generate
+let margin = 2;
+// A Grid we'll initialize in setup()
 let grid;
+// Properties to apply to the island generator
+let islandProperties = {
+    minDim: 5,
+    maxDim: 20,
+    minCount: 6,
+    maxCount: 15
+};
+// The starting chance to break a generation loop early
+let breakChance = 0.1;
+
+let onChance = 0.2;
 
 function setup(){
-    offColor = color(0, 0, 0);
-    onColor = color(255, 255, 255);
-    createCanvas(canvasDimensions, canvasDimensions);
+    // These three colors are declared in grid.js and initialized here
+    waterColor = color(0, 112, 236);
+    landColor = color(128, 208, 16);
+    treeColor = color(0, 168, 0);
+
+    // Standard p5 setup
+    canvas = createCanvas(canvasDimensions, canvasDimensions);
     background(0);
     noStroke();
-    grid = generateGridColors(new Grid(boxes), generationPasses);
+
+    // Create a new boxes x boxes grid
+    // We use canvasDimensions to determine the size of each box
+    grid = new Grid(boxes, canvasDimensions, margin);
+    // Add islands to the grid
+    grid = generateIslands(
+        grid, 
+        islandProperties.minCount, 
+        islandProperties.maxCount, 
+        islandProperties.minDim, 
+        islandProperties.maxDim, 
+        breakChance
+        );
 }
 
 function draw(){
     grid.display();
 }
 
-function generateGridColors(gridToModify, passes) {
-    let modifiedGrid = gridToModify;
-    // Pass 0
-    for(let x = 0; x < modifiedGrid.boxArray.length; x++){
-        for(let y = 0; y < modifiedGrid.boxArray[0].length; y++){
-            if(Math.random() < onChance){
-                modifiedGrid.boxArray[x][y].fillColor = onColor;
+// Save a screenshot by pressing space
+// function keyReleased() {
+//     if(keyCode === 32) {
+//         save(canvas, `images/myCanvas${Date.now()}`);
+//     }
+// }
+
+/**
+ * Generates islands (rectangular chunks of boxes in the "land" state) in the given grid
+ * Uses a random chance to stop early, so it won't always generate the maximum number
+ * Note that these islands *will* overlap!
+ * If we wanted to prevent this, we could save a list of islands and 
+ * compare new ones to the existing ones as well as the main grid
+ * 
+ * @param {Grid} gridToModify The grid to generate islands in
+ * @param {number} maxIslands The maximum number of islands to generate
+ * @param {number} minDim The minimum width or height of an island
+ * @param {number} maxDim The maximum width or height of an island
+ * @param {number} stopChance The chance to stop after each island. Increases with each successive island
+ * 
+ * @returns The grid, now with islands
+ */
+function generateIslands(gridToModify, minIslands, maxIslands, minDim, maxDim, stopChance) {
+    // Loop until we have the maximum number of islands (or we break out early)
+    for(let islands = 0; islands < maxIslands; islands++) {
+        // Define a new island object with upper left corner point and dimensions
+        let newIsland = {
+            cornerX: 0,
+            cornerY: 0,
+            width: 0,
+            height: 0
+        };
+
+        // Generate random coordinates within the grid margins
+        // Make sure there's room for at least a minimally sized island
+        newIsland.cornerX = randomNumberGenerator(gridToModify.lowMargin, gridToModify.highXMargin - minDim);
+        newIsland.cornerY = randomNumberGenerator(gridToModify.lowMargin, gridToModify.highYMargin - minDim);
+
+        // Generate random dimenstions within the min/max
+        // Since the RNG max is exclusive, we add 1 to maxDim here
+        newIsland.width = randomNumberGenerator(minDim, maxDim + 1);
+        newIsland.height = randomNumberGenerator(minDim, maxDim + 1);
+
+        // Make sure the island fits within the margins and resize if necessary
+        if(newIsland.cornerX + newIsland.width > gridToModify.highXMargin) {
+            newIsland.width = gridToModify.highXMargin - newIsland.cornerX;
+        }
+        if(newIsland.cornerY + newIsland.height > gridToModify.highYMargin) {
+            newIsland.height = gridToModify.highYMargin - newIsland.cornerY;
+        }
+
+        // Loop through all the boxes within the newly-created island and set their states to "land"
+        for(let x = newIsland.cornerX; x < newIsland.cornerX + newIsland.width; x++) {
+            for(let y = newIsland.cornerY; y < newIsland.cornerY + newIsland.height; y++) {
+                gridToModify.boxArray[x][y].setState("land");
+            }
+        }
+        
+        // Set the current chance to break the loop based on how close the number of islands is to the max
+        // This looks scary, but we're scaling it so the chance only reaches 1 (100%) when we reach the max
+        if(islands > minIslands && islands < maxIslands) {
+            let currentStopChance = stopChance + ((1 - stopChance) / ((maxIslands - minIslands) - (islands - minIslands)));
+            // Math.random() generates a decimal between [0, 1), so we can compare it to another decimal for a % chance event
+            if(Math.random() < currentStopChance) {
+                console.log(`Stopped after ${islands + 1} islands`);
+                break;
             }
         }
     }
-    // Grow the activated boxes by {passes} generations
-    for(let g = 0; g < passes; g++){
-        modifiedGrid = advanceGeneration(modifiedGrid);
-    }
-    return modifiedGrid;
+
+    return gridToModify;
 }
 
-function advanceGeneration(gridToAdvance) {
-    modifiedGrid = gridToAdvance;
-    for(let x = 0; x < modifiedGrid.boxArray.length; x++){
-        for(let y = 0; y < modifiedGrid.boxArray[0].length; y++){
-            if(modifiedGrid.boxArray[x][y].fillColor === onColor){
-            } else {
-                let flipChance = 0;
-                let neighbors = checkNeighbors(gridToAdvance, x, y);
-                flipChance += neighborModifier * neighbors;
-                if(Math.random() < flipChance){
-                    modifiedGrid.boxArray[x][y].fillColor = onColor;
-                }
+/**
+ * Loops through the given grid and randomly sets boxes to land
+ * If we run this, we can see that there's not a lot of "procedure" here--it's just rolling a die a lot
+ * 
+ * @param {Grid} gridToModify The grid to randomly populate
+ * 
+ * @returns The randomly populated grid
+ */
+function randomizeGrid(gridToModify) {
+    let modifiedGrid = gridToModify;
+    // Loop through each Box in the Grid and randomly assign some of the boxes as land
+    for(let x = gridToModify.lowMargin; x < modifiedGrid.highXMargin; x++){
+        for(let y = gridToModify.lowMargin; y < modifiedGrid.highYMargin; y++){
+            if(Math.random() < onChance){
+                modifiedGrid.boxArray[x][y].setState("land");
             }
         }
     }
@@ -56,27 +145,39 @@ function advanceGeneration(gridToAdvance) {
 }
 
 /**
+ * Checks the cells immediately adjacent to one particular cell in the given grid
+ * Totals the number of neighbors with the given state (diagonal neighbors count as half)
  * 
  * @param {Grid} gridToCheck The grid to evaluate neighbors in
  * @param {number} x The x-position of the cell in question
  * @param {number} y The y-position of the cell in question
+ * @param {string} checkState The state we're looking for in neighbors 
  * 
- * @return {number} The number of active neighbors
+ * @return {number} The number of matching neighbors
  */
-function checkNeighbors(gridToCheck, x, y){
+function checkNeighbors(gridToCheck, x, y, checkState){
     let neighbors = 0;
+    // Loop through the three columns centered on our cell
     for(let a = -1; a < 2; a++){
-        if(x + a < 0 || x + a >= gridToCheck.boxArray.length){
+        // Make sure we're checking columns within the grid
+        if(x + a < 0 || x + a >= gridToCheck.width){
         } else {
+            // Loop through the three rows centered on our cell
             for(let b = -1; b < 2; b++){ 
-                if(y + b < 0 || y + b >= gridToCheck.boxArray.length){
+                // Again, make sure we're staying within the grid
+                // Also make sure we don't count our starting tile
+                if(y + b < 0 || y + b >= gridToCheck.height){
                 } else if(a === 0 && b === 0){
                 } else {
-                    if(gridToCheck.boxArray[x + a][y + b].fillColor === onColor){
+                    // Check the current state of the neighboring cell
+                    if(gridToCheck.boxArray[x + a][y + b].currentState === checkState){
+                        // If the absolute values match, we're looking at a diagonal neighbor, which should have less influence
                         if(Math.abs(a) === Math.abs(b)) {
                             neighbors += 0.5;
                         } else {
+                            // Fully increment neighbors if a non-diagonal neighbor matches
                             neighbors++;
+                            // An ungodly amount of closing brackets
                         }
                     }
                 }
@@ -86,49 +187,12 @@ function checkNeighbors(gridToCheck, x, y){
     return neighbors;
 }
 
-class Grid {
-    /**
-     * 
-     * @param {number} boxesPerRow The desired number of boxes per row/column of the grid 
-     */
-    constructor(boxesPerRow){
-        this.boxArray = [];
-        for(let a = 0; a < boxesPerRow; a++){
-            let temp = [];
-            let dim = canvasDimensions/boxesPerRow;
-            for(let b = 0; b < boxesPerRow; b++){
-                temp.push(new Box(dim * a, dim * b, dim));
-            }
-            this.boxArray.push(temp);
-        }
-    }
-
-    display() {
-        for(let x = 0; x < this.boxArray.length; x++) {
-            for(let y = 0; y < this.boxArray[0].length; y++) {
-                this.boxArray[x][y].drawBox();
-            }
-        }
-    }
-}
-
-class Box {
-    /**
-     * 
-     * @param {number} xPos The horizontal position of the box's upper left corner
-     * @param {number} yPos The vertical position of the box's upper left corner
-     * @param {number} width The box's width
-     * @param {color} color The box's starting color
-     */
-    constructor(xPos, yPos, width, fColor) {
-        this.cornerX = xPos;
-        this.cornerY = yPos;
-        this.dimensions = width;
-        this.fillColor = fColor === undefined ? offColor : fColor;
-    }
-
-    drawBox() {
-        fill(this.fillColor);
-        rect(this.cornerX, this.cornerY, this.dimensions, this.dimensions);
-    }
+/**
+ * Generates a random number in the range [min, max)
+ * 
+ * @param {number} min 
+ * @param {number} max 
+ */
+function randomNumberGenerator(min, max) {
+    return Math.floor((Math.random() * (max - min)) + min);
 }
